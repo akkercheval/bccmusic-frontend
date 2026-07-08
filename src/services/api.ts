e04504b2f0138970a6f2ba26fa740c849362ce5e
Amanda Kay Kercheval <102194663+akkercheval@users.noreply.github.com>
2026-07-08 19:23:38 -0400

    code cleanup

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api-bccmusic.boonecountyin.org',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   withCredentials: true,
 });
 
